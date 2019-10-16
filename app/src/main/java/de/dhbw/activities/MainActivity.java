084e7fb1cd2d8dec821b7a09799d96ee26bbacc3
Nicolas <mail@nicolas-wagner.com>
2019-10-16 22:22:37 +0200

    [NW] basic activity structure

--- app/src/main/java/de/dhbw/activities/MainActivity.java
+++ app/src/main/java/de/dhbw/activities/MainActivity.java
@@ -2,15 +2,28 @@ package de.dhbw.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import de.dhbw.R;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_main);
+        Button playButton = findViewById(R.id.playButtonMain);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gameIntent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(gameIntent);
+            }
+        });
 
     }
 }

478129b86d3fb2fba809d9dc6b545ea80715e21c
Fabian1699 <fabian.bmail@gmail.com>
2019-11-29 23:44:22 +0100

    [DHTD-40] added dropdown to choose difficulties -> we need this to test our use cases with cucumber

--- app/src/main/java/de/dhbw/activities/MainActivity.java
+++ app/src/main/java/de/dhbw/activities/MainActivity.java
@@ -5,9 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 import de.dhbw.R;
+import de.dhbw.util.spinnerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +23,39 @@ public class MainActivity extends AppCompatActivity {
 
         //Initialize Play-Button
         Button playButton = findViewById(R.id.playButtonMain);
-        playButton.setOnClickListener(new View.OnClickListener() {
+
+        AdapterView.OnItemSelectedListener listener = new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                //Go to GameActivity
-                Intent gameIntent = new Intent(MainActivity.this, GameActivity.class);
-                startActivity(gameIntent);
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //go to GameActivity
+                if(parent.getSelectedItem().equals("PLAY"))
+                {
+                    //nothing
+                }
+                else{
+                    Intent gameIntent = new Intent(MainActivity.this, GameActivity.class);
+                    startActivity(gameIntent);
+
+                }
             }
-        });
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        };
+
+        Spinner spinner = findViewById(R.id.spinner_choose_difficulties);
+        spinnerAdapter adapter = new spinnerAdapter(MainActivity.this, R.layout.spinner_item);
+        //adapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
+        adapter.addAll("easy", "medium", "hard");
+        adapter.add("PLAY");
+        spinner.setAdapter(adapter);
+        spinner.setSelection(adapter.getCount());
+
+
+        spinner.setOnItemSelectedListener(listener);
+
 
     }
 }

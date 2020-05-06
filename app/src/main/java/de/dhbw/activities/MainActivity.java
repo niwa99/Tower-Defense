12f9a66b815c30b7a6473b3a5de60731838da09e
thjg <info@thjg.de>
2020-05-06 11:31:50 +0200

    [DHTD-73] Removed stats-reset functionality

--- app/src/main/java/de/dhbw/activities/MainActivity.java
+++ app/src/main/java/de/dhbw/activities/MainActivity.java
@@ -45,22 +45,6 @@ public class MainActivity extends AppCompatActivity {
         //Setup MediaPlayer for music on the MainActivity
         mediaPlayer = MediaPlayer.create(this, R.raw.tower_defense_title_soundtrack);
         initTitleSounds();
-
-        //Setup reset-stats button
-        Button buttonResetStats = findViewById(R.id.button_reset_stats);
-        buttonResetStats.setOnClickListener(view -> {
-            Difficulty difficulty = Difficulty.asDifficulty(viewPager.getCurrentItem());
-            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-            builder.setTitle("Reset Statistics");
-            builder.setMessage("Do you really want to reset the statistics for the difficulty " + difficulty + "?");
-            builder.setPositiveButton("Yes", (dialogInterface, i) -> {
-                resetStatistics(difficulty);
-                viewPager.setAdapter(pagerAdapter);
-                viewPager.setCurrentItem(Difficulty.asNumber(difficulty), false);
-            });
-            builder.setNegativeButton("No", null);
-            builder.create().show();
-        });
     }
 
     private void initTitleSounds() {
@@ -96,31 +80,5 @@ public class MainActivity extends AppCompatActivity {
         mediaPlayer.setLooping(true);
         mediaPlayer.setVolume(100, 100);
         mediaPlayer.start();
-    }
-
-    private void resetStatistics(Difficulty difficulty) {
-        switch (difficulty) {
-            case EASY:
-                PreferenceManager.removeStatisticsValue(PreferenceManager.Statistics.EASY_MAX_WAVE);
-                PreferenceManager.removeStatisticsValue(PreferenceManager.Statistics.EASY_ENEMIES_KILLED);
-                PreferenceManager.removeStatisticsValue(PreferenceManager.Statistics.EASY_BUILT_TOWERS);
-                PreferenceManager.removeStatisticsValue(PreferenceManager.Statistics.EASY_UPGRADES);
-                PreferenceManager.removeStatisticsValue(PreferenceManager.Statistics.EASY_MONEY_SPENT);
-                break;
-            case MEDIUM:
-                PreferenceManager.removeStatisticsValue(PreferenceManager.Statistics.MEDIUM_MAX_WAVE);
-                PreferenceManager.removeStatisticsValue(PreferenceManager.Statistics.MEDIUM_ENEMIES_KILLED);
-                PreferenceManager.removeStatisticsValue(PreferenceManager.Statistics.MEDIUM_BUILT_TOWERS);
-                PreferenceManager.removeStatisticsValue(PreferenceManager.Statistics.MEDIUM_UPGRADES);
-                PreferenceManager.removeStatisticsValue(PreferenceManager.Statistics.MEDIUM_MONEY_SPENT);
-                break;
-            case HARD:
-                PreferenceManager.removeStatisticsValue(PreferenceManager.Statistics.HARD_MAX_WAVE);
-                PreferenceManager.removeStatisticsValue(PreferenceManager.Statistics.HARD_ENEMIES_KILLED);
-                PreferenceManager.removeStatisticsValue(PreferenceManager.Statistics.HARD_BUILT_TOWERS);
-                PreferenceManager.removeStatisticsValue(PreferenceManager.Statistics.HARD_UPGRADES);
-                PreferenceManager.removeStatisticsValue(PreferenceManager.Statistics.HARD_MONEY_SPENT);
-                break;
-        }
     }
 }

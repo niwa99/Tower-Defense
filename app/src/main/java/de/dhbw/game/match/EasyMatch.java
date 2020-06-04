be36afcfafbcfac3fe23cc8044cd6b4c20964222
thjg <info@thjg.de>
2020-06-04 18:33:42 +0200

    [DHTD-84] add car enemies to EasyMatch

--- app/src/main/java/de/dhbw/game/match/EasyMatch.java
+++ app/src/main/java/de/dhbw/game/match/EasyMatch.java
@@ -10,13 +10,16 @@ public class EasyMatch extends AMatch {
     private static final int waveTime = 45;
     private static final int waveSpeed = 2000;
     private static final int startMoney = 100;
-    private static WaveComposition firstWaveTankL1 = new WaveComposition(EnemyType.TANK, 1, 8);
+    private static WaveComposition firstWaveCarL1 = new WaveComposition(EnemyType.CAR, 1, 3);
+    private static WaveComposition firstWaveTankL1 = new WaveComposition(EnemyType.TANK, 1, 6);
     private static WaveComposition firstWaveTankL2 = new WaveComposition(EnemyType.TANK, 2, 2);
     private static WaveComposition secondWaveTankL1 = new WaveComposition(EnemyType.TANK, 1, 5);
     private static WaveComposition secondWaveTankL2 = new WaveComposition(EnemyType.TANK, 2, 5);
     private static WaveComposition thirdWaveTankL2 = new WaveComposition(EnemyType.TANK, 2, 10);
+    private static WaveComposition thirdWaveCarL2 = new WaveComposition(EnemyType.CAR, 2, 5);
     private static WaveComposition fourthWaveTankL2 = new WaveComposition(EnemyType.TANK, 2, 5);
     private static WaveComposition fourthWaveTankL3 = new WaveComposition(EnemyType.TANK, 3, 5);
+    private static WaveComposition fifthWaveCarL3 = new WaveComposition(EnemyType.CAR, 3, 5);
     private static WaveComposition fithWaveTankL3 = new WaveComposition(EnemyType.TANK, 3, 10);
 
 
@@ -28,7 +31,7 @@ public class EasyMatch extends AMatch {
     public void create(GameActivity gameActivity) {
 
         Wave firstWave = new Wave(waveSpeed);
-        firstWave.addWaveCompositions(firstWaveTankL1, firstWaveTankL2);
+        firstWave.addWaveCompositions(firstWaveCarL1, firstWaveTankL1, firstWaveTankL2);
         firstWave.generate(gameActivity);
 
         Wave secondWave = new Wave(waveSpeed);
@@ -36,7 +39,7 @@ public class EasyMatch extends AMatch {
         secondWave.generate(gameActivity);
 
         Wave thirdWave = new Wave(waveSpeed);
-        thirdWave.addWaveCompositions(thirdWaveTankL2);
+        thirdWave.addWaveCompositions(thirdWaveCarL2, thirdWaveTankL2);
         thirdWave.generate(gameActivity);
 
         Wave fourthWave = new Wave(waveSpeed);
@@ -44,7 +47,7 @@ public class EasyMatch extends AMatch {
         fourthWave.generate(gameActivity);
 
         Wave fithWave = new Wave(waveSpeed);
-        fithWave.addWaveCompositions(fithWaveTankL3);
+        fithWave.addWaveCompositions(fifthWaveCarL3, fithWaveTankL3);
         fithWave.generate(gameActivity);
 
         addWaves(firstWave, secondWave, thirdWave, fourthWave, fithWave);

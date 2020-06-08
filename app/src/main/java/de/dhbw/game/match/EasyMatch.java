14331e66b2061a9e9ba020790b48eb266fc32f46
Charon1502 <luca-rutschmann@gmx.de>
2020-06-08 18:30:04 +0200

    [DHTD-88]: Design the easy match

--- app/src/main/java/de/dhbw/game/match/EasyMatch.java
+++ app/src/main/java/de/dhbw/game/match/EasyMatch.java
@@ -10,34 +10,50 @@ public class EasyMatch extends AMatch {
     private static final int waveTime = 50;
     private static final int waveSpeed = 1500;
     private static final int startMoney = 35;
-    private static WaveComposition firstWaveCarL1 = new WaveComposition(EnemyType.CAR, 1, 5);
-    private static WaveComposition secondWaveTankL1 = new WaveComposition(EnemyType.TANK, 1, 8);
-    private static WaveComposition thirdWaveCarL2 = new WaveComposition(EnemyType.CAR, 2, 8);
-    private static WaveComposition fourthWaveTankL2 = new WaveComposition(EnemyType.TANK, 2, 10);
-    private static WaveComposition fifthWavePlaneL1 = new WaveComposition(EnemyType.PLANE, 1, 3);
-    private static WaveComposition w6_TankL3 = new WaveComposition(EnemyType.TANK, 2, 15);
-    private static WaveComposition w7_CarL3 = new WaveComposition(EnemyType.CAR, 3, 5);
-    private static WaveComposition w7_TankL3 = new WaveComposition(EnemyType.TANK, 3, 10);
-    private static WaveComposition w8_TankL3 = new WaveComposition(EnemyType.TANK, 3, 20);
-    private static WaveComposition w9_TankL3 = new WaveComposition(EnemyType.TANK, 3, 10);
-    private static WaveComposition w9_CarL3 = new WaveComposition(EnemyType.CAR, 3, 10);
-    private static WaveComposition w10_TankL3 = new WaveComposition(EnemyType.TANK, 3, 5);
-    private static WaveComposition w10_TankBoss = new WaveComposition(EnemyType.BOSS_TANK, 1, 1);
-    private static WaveComposition w10_CarL3 = new WaveComposition(EnemyType.CAR, 3, 5);
-    private static WaveComposition w11_TankL3 = new WaveComposition(EnemyType.TANK, 3, 10);
-    private static WaveComposition w11_CarL3 = new WaveComposition(EnemyType.CAR, 3, 10);
-    private static WaveComposition w11_PlaneL3 = new WaveComposition(EnemyType.PLANE, 3, 10);
-    private static WaveComposition w12_TankL3 = new WaveComposition(EnemyType.TANK, 3, 20);
-    private static WaveComposition w13_CarL3 = new WaveComposition(EnemyType.CAR, 3, 20);
-    private static WaveComposition w14_TankL3 = new WaveComposition(EnemyType.TANK, 3, 10);
-    private static WaveComposition w14_CarL3 = new WaveComposition(EnemyType.CAR, 3, 10);
-    private static WaveComposition w15_TankL3 = new WaveComposition(EnemyType.TANK, 3, 10);
-    private static WaveComposition w15_CarL3 = new WaveComposition(EnemyType.CAR, 3, 10);
-    private static WaveComposition w16_PlaneL3 = new WaveComposition(EnemyType.PLANE, 3, 20);
-    private static WaveComposition w17_TankL3 = new WaveComposition(EnemyType.TANK, 3, 10);
-    private static WaveComposition w17_CarL3 = new WaveComposition(EnemyType.CAR, 3, 10);
-    private static WaveComposition w30_TankBoss = new WaveComposition(EnemyType.BOSS_TANK, 3, 10);
-    // MAX 20 Gegner pro waves, Enemy level generieren, Boss ändern evtl
+    private static WaveComposition firstWaveCar = new WaveComposition(EnemyType.CAR, 1, 3);
+    private static WaveComposition secondWaveTank = new WaveComposition(EnemyType.TANK, 1, 3);
+    private static WaveComposition thirdWaveCar = new WaveComposition(EnemyType.CAR, 2, 5);
+    private static WaveComposition fourthWaveTank = new WaveComposition(EnemyType.TANK, 2, 6);
+    private static WaveComposition fifthWavePlane = new WaveComposition(EnemyType.PLANE, 1, 6);
+    private static WaveComposition w6_Tank = new WaveComposition(EnemyType.TANK, 2, 8);
+    private static WaveComposition w7_Car = new WaveComposition(EnemyType.CAR, 3, 3);
+    private static WaveComposition w7_Tank = new WaveComposition(EnemyType.TANK, 3, 4);
+    private static WaveComposition w8_Tank = new WaveComposition(EnemyType.TANK, 3, 9);
+    private static WaveComposition w9_Tank = new WaveComposition(EnemyType.TANK, 3, 8);
+    private static WaveComposition w9_Car = new WaveComposition(EnemyType.CAR, 3, 6);
+    private static WaveComposition w10_Tank = new WaveComposition(EnemyType.TANK, 3, 3);
+    private static WaveComposition w10_TankBoss = new WaveComposition(EnemyType.BOSS_TANK, 2, 1);
+    private static WaveComposition w10_Car = new WaveComposition(EnemyType.CAR, 3, 3);
+    private static WaveComposition w11_Tank = new WaveComposition(EnemyType.TANK, 3, 5);
+    private static WaveComposition w11_Car = new WaveComposition(EnemyType.CAR, 3, 5);
+    private static WaveComposition w11_Plane = new WaveComposition(EnemyType.PLANE, 3, 5);
+    private static WaveComposition w12_Tank = new WaveComposition(EnemyType.TANK, 3, 13);
+    private static WaveComposition w13_Car = new WaveComposition(EnemyType.CAR, 3, 14);
+    private static WaveComposition w14_Tank = new WaveComposition(EnemyType.TANK, 3, 8);
+    private static WaveComposition w14_Car = new WaveComposition(EnemyType.CAR, 3, 8);
+    private static WaveComposition w15_Tank = new WaveComposition(EnemyType.TANK, 3, 4);
+    private static WaveComposition w15_Car = new WaveComposition(EnemyType.CAR, 3, 4);
+    private static WaveComposition w16_Plane = new WaveComposition(EnemyType.PLANE, 3, 15);
+    private static WaveComposition w17_Tank = new WaveComposition(EnemyType.TANK, 4, 8);
+    private static WaveComposition w17_Car = new WaveComposition(EnemyType.CAR, 4, 9);
+    private static WaveComposition w18_Tank = new WaveComposition(EnemyType.TANK, 5, 5);
+    private static WaveComposition w18_Car = new WaveComposition(EnemyType.CAR, 5, 5);
+    private static WaveComposition w18_Plane = new WaveComposition(EnemyType.PLANE, 6, 5);
+    private static WaveComposition w19_Tank = new WaveComposition(EnemyType.TANK, 6, 12);
+    private static WaveComposition w20_TankBoss = new WaveComposition(EnemyType.BOSS_TANK, 5, 5);
+    private static WaveComposition w21_Car = new WaveComposition(EnemyType.CAR, 7, 16);
+    private static WaveComposition w22_Tank = new WaveComposition(EnemyType.TANK, 6, 10);
+    private static WaveComposition w23_Plane = new WaveComposition(EnemyType.PLANE, 6, 10);
+    private static WaveComposition w24_Tank = new WaveComposition(EnemyType.TANK, 8, 3);
+    private static WaveComposition w24_Car = new WaveComposition(EnemyType.CAR, 8, 3);
+    private static WaveComposition w24_Plane = new WaveComposition(EnemyType.PLANE, 8, 3);
+    private static WaveComposition w25_TankBoss = new WaveComposition(EnemyType.BOSS_TANK, 6, 1);
+    private static WaveComposition w26_Car = new WaveComposition(EnemyType.CAR, 10, 8);
+    private static WaveComposition w27_Tank = new WaveComposition(EnemyType.TANK, 12, 6);
+    private static WaveComposition w28_Tank = new WaveComposition(EnemyType.TANK, 10, 7);
+    private static WaveComposition w28_Car = new WaveComposition(EnemyType.CAR, 10, 6);
+    private static WaveComposition w29_Plane = new WaveComposition(EnemyType.PLANE, 12, 10);
+    private static WaveComposition w30_TankBoss = new WaveComposition(EnemyType.BOSS_TANK, 4, 10);
 
     public EasyMatch() {
         super(waveTime, startMoney, Difficulty.EASY);
@@ -47,119 +63,119 @@ public class EasyMatch extends AMatch {
     public void create(GameActivity gameActivity) {
 
         Wave firstWave = new Wave(waveSpeed);
-        firstWave.addWaveCompositions(firstWaveCarL1);
+        firstWave.addWaveCompositions(firstWaveCar);
         firstWave.generate(gameActivity);
 
         Wave secondWave = new Wave(waveSpeed);
-        secondWave.addWaveCompositions(secondWaveTankL1);
+        secondWave.addWaveCompositions(secondWaveTank);
         secondWave.generate(gameActivity);
 
         Wave thirdWave = new Wave(waveSpeed);
-        thirdWave.addWaveCompositions(thirdWaveCarL2);
+        thirdWave.addWaveCompositions(thirdWaveCar);
         thirdWave.generate(gameActivity);
 
         Wave fourthWave = new Wave(waveSpeed);
-        fourthWave.addWaveCompositions(fourthWaveTankL2);
+        fourthWave.addWaveCompositions(fourthWaveTank);
         fourthWave.generate(gameActivity);
 
         Wave fithWave = new Wave(waveSpeed);
-        fithWave.addWaveCompositions(fifthWavePlaneL1);
+        fithWave.addWaveCompositions(fifthWavePlane);
         fithWave.generate(gameActivity);
 
         Wave wave6 = new Wave(waveSpeed);
-        wave6.addWaveCompositions(w6_TankL3);
+        wave6.addWaveCompositions(w6_Tank);
         wave6.generate(gameActivity);
 
         Wave wave7 = new Wave(waveSpeed);
-        wave7.addWaveCompositions(w7_CarL3, w7_TankL3, w7_CarL3);
+        wave7.addWaveCompositions(w7_Car, w7_Tank, w7_Car);
         wave7.generate(gameActivity);
 
         Wave wave8 = new Wave(waveSpeed);
-        wave8.addWaveCompositions(w8_TankL3);
+        wave8.addWaveCompositions(w8_Tank);
         wave8.generate(gameActivity);
 
         Wave wave9 = new Wave(waveSpeed);
-        wave9.addWaveCompositions(w9_TankL3, w9_CarL3);
+        wave9.addWaveCompositions(w9_Tank, w9_Car);
         wave9.generate(gameActivity);
 
         Wave wave10 = new Wave(waveSpeed);
-        wave10.addWaveCompositions(w10_TankL3, w10_TankBoss, w10_CarL3);
+        wave10.addWaveCompositions(w10_Tank, w10_TankBoss, w10_Car);
         wave10.generate(gameActivity);
 
         Wave wave11 = new Wave(waveSpeed);
-        wave11.addWaveCompositions(w11_TankL3, w11_CarL3, w11_PlaneL3);
+        wave11.addWaveCompositions(w11_Tank, w11_Car, w11_Plane);
         wave11.generate(gameActivity);
 
         Wave wave12 = new Wave(waveSpeed);
-        wave12.addWaveCompositions(w12_TankL3);
+        wave12.addWaveCompositions(w12_Tank);
         wave12.generate(gameActivity);
 
         Wave wave13 = new Wave(waveSpeed);
-        wave13.addWaveCompositions(w13_CarL3);
+        wave13.addWaveCompositions(w13_Car);
         wave13.generate(gameActivity);
 
         Wave wave14 = new Wave(waveSpeed);
-        wave14.addWaveCompositions(w14_TankL3, w14_CarL3);
+        wave14.addWaveCompositions(w14_Tank, w14_Car);
         wave14.generate(gameActivity);
 
         Wave wave15 = new Wave(waveSpeed);
-        wave15.addWaveCompositions(w15_CarL3, w15_TankL3, w15_CarL3, w15_TankL3, w15_CarL3);
+        wave15.addWaveCompositions(w15_Tank, w15_Car, w15_Tank, w15_Car);
         wave15.generate(gameActivity);
 
         Wave wave16 = new Wave(waveSpeed);
-        wave16.addWaveCompositions(w16_PlaneL3);
+        wave16.addWaveCompositions(w16_Plane);
         wave16.generate(gameActivity);
 
         Wave wave17 = new Wave(waveSpeed);
-        wave17.addWaveCompositions(w17_TankL3, w17_CarL3);
+        wave17.addWaveCompositions(w17_Tank, w17_Car);
         wave17.generate(gameActivity);
 
         Wave wave18 = new Wave(waveSpeed);
-        wave18.addWaveCompositions();
+        wave18.addWaveCompositions(w18_Tank, w18_Car, w18_Plane);
         wave18.generate(gameActivity);
 
         Wave wave19 = new Wave(waveSpeed);
-        wave19.addWaveCompositions();
+        wave19.addWaveCompositions(w19_Tank);
         wave19.generate(gameActivity);
 
         Wave wave20 = new Wave(waveSpeed);
-        wave20.addWaveCompositions();
+        wave20.addWaveCompositions(w20_TankBoss);
         wave20.generate(gameActivity);
 
         Wave wave21 = new Wave(waveSpeed);
-        wave21.addWaveCompositions();
+        wave21.addWaveCompositions(w21_Car);
         wave21.generate(gameActivity);
 
         Wave wave22 = new Wave(waveSpeed);
-        wave22.addWaveCompositions();
+        wave22.addWaveCompositions(w22_Tank);
         wave22.generate(gameActivity);
 
         Wave wave23 = new Wave(waveSpeed);
-        wave23.addWaveCompositions();
+        wave23.addWaveCompositions(w23_Plane);
         wave23.generate(gameActivity);
 
         Wave wave24 = new Wave(waveSpeed);
-        wave24.addWaveCompositions();
+        wave24.addWaveCompositions(w24_Tank, w24_Car, w24_Plane, w24_Tank, w24_Car);
         wave24.generate(gameActivity);
 
         Wave wave25 = new Wave(waveSpeed);
-        wave25.addWaveCompositions();
+        wave25.addWaveCompositions(w25_TankBoss);
         wave25.generate(gameActivity);
 
         Wave wave26 = new Wave(waveSpeed);
-        wave26.addWaveCompositions();
+        wave26.addWaveCompositions(w26_Car);
         wave26.generate(gameActivity);
 
         Wave wave27 = new Wave(waveSpeed);
-        wave27.addWaveCompositions();
+        wave27.addWaveCompositions(w27_Tank);
         wave27.generate(gameActivity);
 
         Wave wave28 = new Wave(waveSpeed);
-        wave28.addWaveCompositions();
+        wave28.addWaveCompositions(w28_Tank, w28_Car);
         wave28.generate(gameActivity);
 
         Wave wave29 = new Wave(waveSpeed);
-        wave29.addWaveCompositions();
+        wave29.addWaveCompositions(w29_Plane);
         wave29.generate(gameActivity);
 
         Wave wave30 = new Wave(waveSpeed);
